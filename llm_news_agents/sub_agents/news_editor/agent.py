7adbb67f116af1831c8f9df2df8adebc8a116235
AnthonyML <112323852+anthonymalumbe@users.noreply.github.com>
2025-11-30 22:02:18 +0000

    Update agent.py

--- llm_news_agents/sub_agents/news_editor/agent.py
+++ llm_news_agents/sub_agents/news_editor/agent.py
@@ -139,7 +139,7 @@ async def final_processing_callback(
     return llm_response
 
 news_editor_agent = Agent(
-    model='gemini-1.5-flash',
+    model='gemini-2.5-flash-lite',
     name='news_editor_agent',
     instruction=prompt.news_editor_PROMPT,
     # Added Google Search for the final grounding step
